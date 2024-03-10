22550f7fed83bc68f6d1b751ce2bd8cf76ac8471
fellenbrais1 <106751977+fellenbrais1@users.noreply.github.com>
2024-03-10 18:43:10 +0900

    Upgrades 'map_maker.py' Renamed some of the rooms for easier readability Changed the order to code elements for readability Added a new example map using more 'null_room's

--- map_maker.py
+++ map_maker.py
@@ -5,14 +5,12 @@
 # These are the components all rooms are made of, they are not very readable /
 # yet but this could be improved in the future
 # Tab these out if you need to see how the look next to one another
-top_wall = " _________"
-top_wall_open = " ___   ___"
+top_wall = " ---------"
+top_wall_open = " ---   ---"
 side_wall = "|         |"
 side_wall_open_left = "          |"
 side_wall_open_both = "           "
 side_wall_open_right = "|          "
-bottom_wall = " ---------"
-bottom_wall_open = " ---   ---"
 room_null = "          "
 
 # These are lists of the components each type of room is made out of, all /
@@ -30,7 +28,7 @@ open_top = [
     side_wall,
     side_wall,
     side_wall,
-    bottom_wall,
+    top_wall,
 ]
 
 open_left = [
@@ -57,7 +55,7 @@ open_bottom = [
     top_wall_open,
 ]
 
-open_left_top = [
+open_top_left = [
     top_wall_open,
     side_wall,
     side_wall_open_left,
@@ -65,36 +63,12 @@ open_left_top = [
     top_wall,
 ]
 
-open_left_bottom = [
-    top_wall,
-    side_wall,
-    side_wall_open_left,
-    side_wall,
-    top_wall_open
-]
-
-open_left_right = [
-    top_wall,
-    side_wall,
-    side_wall_open_both,
-    side_wall,
-    bottom_wall,
-]
-
-open_right_top = [
+open_top_right = [
     top_wall_open,
     side_wall,
     side_wall_open_right,
     side_wall,
     top_wall,
-]
-
-open_right_bottom = [
-    top_wall,
-    side_wall,
-    side_wall_open_right,
-    side_wall,
-    bottom_wall_open,
 ]
 
 open_top_bottom = [
@@ -113,15 +87,7 @@ open_top_left_right = [
     top_wall
 ]
 
-open_bottom_left_right = [
-    top_wall,
-    side_wall,
-    side_wall_open_both,
-    side_wall,
-    top_wall_open
-]
-
-open_top_bottom_left = [
+open_top_left_bottom = [
     top_wall_open,
     side_wall,
     side_wall_open_left,
@@ -129,12 +95,44 @@ open_top_bottom_left = [
     top_wall_open
 ]
 
-open_top_bottom_right = [
+open_top_right_bottom = [
     top_wall_open,
     side_wall,
     side_wall_open_right,
     side_wall,
     top_wall_open
+]
+
+open_left_right = [
+    top_wall,
+    side_wall,
+    side_wall_open_both,
+    side_wall,
+    top_wall,
+]
+
+open_left_bottom = [
+    top_wall,
+    side_wall,
+    side_wall_open_left,
+    side_wall,
+    top_wall_open
+]
+
+open_left_right_bottom = [
+    top_wall,
+    side_wall,
+    side_wall_open_both,
+    side_wall,
+    top_wall_open
+]
+
+open_right_bottom = [
+    top_wall,
+    side_wall,
+    side_wall_open_right,
+    side_wall,
+    top_wall_open,
 ]
 
 open_all = [
@@ -162,52 +160,61 @@ available_rooms = [
     open_left,
     open_right,
     open_bottom,
-    open_left_top,
-    open_left_right,
-    open_left_bottom,
-    open_right_top,
-    open_right_bottom,
+    open_top_left,
+    open_top_right,
     open_top_bottom,
     open_top_left_right,
-    open_bottom_left_right,
-    open_top_bottom_left,
-    open_top_bottom_right,
+    open_top_left_bottom,
+    open_top_right_bottom,
+    open_left_right,
+    open_left_bottom,
+    open_left_right_bottom,
+    open_right_bottom,
     open_all,
     null_room,
 ]
 
-# Building row components for the map 'understreets' which is 'map_01'
+# The map building process:
+# Maps need an id specified, then compositions for their rows, then which /
+# rows are present in that map as below
+
+# Added an 'id' variable to print where you are, I think this could be /
+# included somewhere else as it just adds something else to worry about as it /
+# is now
+map_01_id = "understruts"
+
+# Building row components for the map 'understruts':
 # Each row has to have the same number of rooms otherwise it stops working /
 # correctly, this can be fixed later. For now, pad empty spaces with /
 # 'null_room's to make the row lengths equal
 map_01_row_1 = open_right_bottom, open_left_right, open_left_bottom, \
                null_room,
 map_01_row_2 = open_top, null_room, open_top_bottom, open_bottom,
-map_01_row_3 = open_right, open_bottom_left_right, open_left_top, \
+map_01_row_3 = open_right, open_left_right_bottom, open_top_left, \
                open_top_bottom,
-map_01_row_4 = null_room, open_right_top, open_left_right, open_left_top,
+map_01_row_4 = null_room, open_top_right, open_left_right, open_top_left,
 
 # A composite of the rows in 'understreets' for 'map_printer()' to use
-map_01_understreets = [
+map_01_understruts = [
     map_01_row_1,
     map_01_row_2,
     map_01_row_3,
     map_01_row_4,
 ]
 
-# Added an 'id' variable to print where you are, I think this could be /
-# included somewhere else as it just adds something else to worry about as it /
-# is now
-map_01_id = "understreets"
+# Playing around building other maps, it might be easier to plan out maps on /
+# excel or something, so the grid is more easily visible, bit this isn't too /
+# difficult so far
 
-# Playing around building another map
+map_02_id = "hat factory"
+
 map_02_row_1 = open_bottom, open_bottom, null_room, open_bottom
 map_02_row_2 = open_top_bottom, open_top_bottom, open_bottom, open_top_bottom
-map_02_row_3 = open_top_bottom, open_top_bottom, open_right_top, \
-               open_top_bottom_left
-map_02_row_4 = open_top_bottom, open_right_top, open_left_right, \
-               open_top_bottom_left
-map_02_row_5 = open_right_top, open_left_right, open_left_right, open_left_top
+map_02_row_3 = open_top_bottom, open_top_bottom, open_top_right, \
+               open_top_left_bottom
+map_02_row_4 = open_top_bottom, open_top_right, open_left_right, \
+               open_top_left_bottom
+map_02_row_5 = open_top_right, open_left_right, open_left_right, open_top_left
 
 map_02_hat_factory = [
     map_02_row_1,
@@ -217,7 +224,21 @@ map_02_hat_factory = [
     map_02_row_5
 ]
 
-map_02_id = "hat factory"
+map_03_id = "snake temple"
+
+map_03_row_1 = open_right, open_left_right, open_left_bottom,
+map_03_row_2 = null_room, null_room, open_top_bottom,
+map_03_row_3 = null_room, open_right_bottom, open_top_left,
+map_03_row_4 = open_right_bottom, open_top_left, null_room,
+map_03_row_5 = open_top, null_room, null_room,
+
+map_03_snake_temple = [
+    map_03_row_1,
+    map_03_row_2,
+    map_03_row_3,
+    map_03_row_4,
+    map_03_row_5,
+]
 
 # 'map_printer()' looks at the map provided to it and generates a string to /
 # print for each line of all the rooms in a row. Then it prints this and moves /
@@ -242,5 +263,6 @@ def map_printer(map_provided, map_id_provided):
 
 
 # call 'map_printer()' with the specified map you want
-map_printer(map_01_understreets, map_01_id)
+map_printer(map_01_understruts, map_01_id)
 map_printer(map_02_hat_factory, map_02_id)
+map_printer(map_03_snake_temple, map_03_id)
